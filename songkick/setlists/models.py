87ed5cfb7b5ad596ba8e2321845bb3962f4b82fe
mattdennewitz <mattdennewitz@gmail.com>
2010-11-27 11:19:48 -0600

    added setlist model docstrings

--- songkick/setlists/models.py
+++ songkick/setlists/models.py
@@ -4,21 +4,45 @@ from songkick import fields
 
 
 class SongkickSetlistItem(SongkickModel):
-    song_title = fields.Field(mapping='name')
+    """A setlist entry.
+
+    :param song_title: Title of the song or piece
+    :param encore: Boolean value stating if this was part of an encore
+    """
+
+    title = fields.Field(mapping='name')
     encore = fields.BooleanField()
 
 
 class SongkickSetlistArtist(SongkickModel):
-    """Artist representations are not consistent between resources,
+    """Setlist artist summary.
+
+    :param id: Songkick artist id
+    :param display_name: Artist's name
+    :param songkick_uri: Songkick artist detail uri
+    :param identifiers: A list of :ref:`SongkickArtistIdentifier` objects
+
+    .. note:: Artist representations are not consistent between resources,
     so we have to redefine a simpler artist model for setlists.
     """
+
     id = fields.Field()
     display_name = fields.Field(mapping='displayName')
-    identifiers = fields.ListField(fields.ObjectField(SongkickArtistIdentifier))
     songkick_uri = fields.Field(mapping='uri')
+    identifiers = fields.ListField(fields.ObjectField(SongkickArtistIdentifier))
 
 
 class SongkickSetlist(SongkickModel):
+    """An event's setlist.
+
+    :param id: Songkick setlist id
+    :param display_name: Setlist name
+    :param artist: :ref:`SongkickSetlistArtist` object representing the
+                   performing artist
+    :param setlist: Songs or pieces performed in this set, as a list of
+                    :ref:`SongkickSetlistItem` objects.
+    """
+
     id = fields.Field()
     display_name = fields.Field(mapping='displayName')
     artist = fields.ObjectField(SongkickSetlistArtist)
